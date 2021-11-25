ddff655b2687b53484434a9bf61438ac65e42622
Sergey Vilgelm <sergey@vilgelm.com>
2021-11-25 09:48:34 -0800

    fix simple test and add docker based (#32)

--- examples/simple/users_test.go
+++ examples/simple/users_test.go
@@ -1,28 +1,38 @@
-package simple
+package simple_test
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
+	"github.com/sv-tools/mongoifc"
+	"github.com/sv-tools/mongoifc/examples/simple"
 	gomockMocks "github.com/sv-tools/mongoifc/mocks/gomock"
 	mockeryMocks "github.com/sv-tools/mongoifc/mocks/mockery"
 )
 
 func TestGetAdmins(t *testing.T) {
-	expectedUsers := []*User{
+	t.Parallel()
+
+	expectedUsers := []*simple.User{
 		{Name: "foo", Active: true, IsAdmin: true},
 		{Name: "bar", Active: true, IsAdmin: true},
 	}
 	ctx := context.Background()
 
 	t.Run("mockery", func(t *testing.T) {
+		t.Parallel()
+
 		cur := &mockeryMocks.Cursor{}
-		cur.On("Decode", mock.Anything).Run(func(args mock.Arguments) {
-			users := args[0].(*[]*User)
+		cur.On("All", ctx, mock.Anything).Run(func(args mock.Arguments) {
+			users := args[1].(*[]*simple.User)
 			*users = append(*users, expectedUsers...)
 		}).Return(nil)
 
@@ -30,20 +40,22 @@ func TestGetAdmins(t *testing.T) {
 		col.On("Find", ctx, mock.AnythingOfType("User")).Return(cur, nil)
 
 		db := &mockeryMocks.Database{}
-		db.On("Collection", UsersCollection).Return(col)
+		db.On("Collection", simple.UsersCollection).Return(col)
 
-		users, err := GetAdmins(ctx, db)
+		users, err := simple.GetAdmins(ctx, db)
 		require.NoError(t, err)
 		require.Equal(t, expectedUsers, users)
 	})
 
 	t.Run("gomock", func(t *testing.T) {
+		t.Parallel()
+
 		ctrl := gomock.NewController(t)
 		defer ctrl.Finish()
 
 		cur := gomockMocks.NewMockCursor(ctrl)
-		cur.EXPECT().Decode(gomock.Any()).Do(func(arg interface{}) {
-			users := arg.(*[]*User)
+		cur.EXPECT().All(ctx, gomock.Any()).Do(func(ctx context.Context, arg interface{}) {
+			users := arg.(*[]*simple.User)
 			*users = append(*users, expectedUsers...)
 		}).Return(nil)
 
@@ -51,10 +63,45 @@ func TestGetAdmins(t *testing.T) {
 		col.EXPECT().Find(ctx, gomock.Any()).Return(cur, nil)
 
 		db := gomockMocks.NewMockDatabase(ctrl)
-		db.EXPECT().Collection(UsersCollection).Return(col)
+		db.EXPECT().Collection(simple.UsersCollection).Return(col)
 
-		users, err := GetAdmins(ctx, db)
+		users, err := simple.GetAdmins(ctx, db)
 		require.NoError(t, err)
+		require.Equal(t, expectedUsers, users)
+	})
+
+	t.Run("docker", func(t *testing.T) {
+		t.Parallel()
+
+		uri := os.Getenv("MONGO_URI")
+		require.NotEmpty(t, uri)
+
+		opt := options.Client().ApplyURI(uri)
+		cl, err := mongoifc.NewClient(opt)
+		require.NoError(t, err)
+		require.NotNil(t, cl)
+
+		err = cl.Connect(ctx)
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			require.NoError(t, cl.Disconnect(ctx))
+		})
+
+		db := cl.Database(fmt.Sprintf("simple_%d", time.Now().Unix()))
+		res, err := db.Collection(simple.UsersCollection).InsertMany(ctx, []interface{}{
+			&simple.User{Name: "blocked admin", Active: false, IsAdmin: true},
+			&simple.User{Name: "active non-admin", Active: true, IsAdmin: false},
+			expectedUsers[0],
+			expectedUsers[1],
+		})
+		require.NoError(t, err)
+		require.Len(t, res.InsertedIDs, 4)
+
+		users, err := simple.GetAdmins(ctx, db)
+		require.NoError(t, err)
+		for _, u := range users {
+			u.ID = ""
+		}
 		require.Equal(t, expectedUsers, users)
 	})
 }
