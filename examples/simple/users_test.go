2884d523c99dacbd9f1153ad35bf83f046c4034d
Sergey Vilgelm <sergey@vilgelm.com>
2025-02-28 12:42:22 -0800

    get rid of Makefile
    - remove Makefile
    - add TestMain to run mongodb in docker
    - few other fixes

--- examples/simple/users_test.go
+++ examples/simple/users_test.go
@@ -3,7 +3,6 @@ package simple_test
 import (
 	"context"
 	"fmt"
-	"os"
 	"testing"
 	"time"
 
@@ -20,30 +19,27 @@ import (
 	mockeryMocks "github.com/sv-tools/mongoifc/mocks/mockery"
 )
 
-var (
-	expectedUsers = []simple.User{
-		{Name: "foo", Active: true, IsAdmin: true},
-		{Name: "bar", Active: true, IsAdmin: true},
-	}
-)
+var expectedUsers = []simple.User{
+	{Name: "foo", Active: true, IsAdmin: true},
+	{Name: "bar", Active: true, IsAdmin: true},
+}
 
 func TestUsersWorkflow(t *testing.T) {
 	t.Parallel()
-	ctx := context.Background()
 
 	t.Run("mockery", func(t *testing.T) {
 		t.Parallel()
 
 		cur := &mockeryMocks.Cursor{}
 		defer cur.AssertExpectations(t)
-		cur.On("All", ctx, mock.Anything).Run(func(args mock.Arguments) {
+		cur.On("All", t.Context(), mock.Anything).Run(func(args mock.Arguments) {
 			users := args[1].(*[]simple.User)
 			*users = append(*users, expectedUsers...)
 		}).Return(nil)
 
 		col := &mockeryMocks.Collection{}
 		defer col.AssertExpectations(t)
-		col.On("InsertMany", ctx, mock.Anything).Return(
+		col.On("InsertMany", t.Context(), mock.Anything).Return(
 			&mongo.InsertManyResult{
 				InsertedIDs: []interface{}{
 					primitive.NewObjectID(),
@@ -54,8 +50,8 @@ func TestUsersWorkflow(t *testing.T) {
 			},
 			nil,
 		)
-		col.On("Find", ctx, mock.AnythingOfType("User")).Return(cur, nil)
-		col.On("DeleteMany", ctx, mock.AnythingOfType("primitive.M")).Return(
+		col.On("Find", t.Context(), mock.AnythingOfType("User")).Return(cur, nil)
+		col.On("DeleteMany", t.Context(), mock.AnythingOfType("primitive.M")).Return(
 			&mongo.DeleteResult{
 				DeletedCount: 4,
 			},
@@ -76,13 +72,13 @@ func TestUsersWorkflow(t *testing.T) {
 		defer ctrl.Finish()
 
 		cur := gomockMocks.NewMockCursor(ctrl)
-		cur.EXPECT().All(ctx, gomock.Any()).Do(func(ctx context.Context, arg interface{}) {
+		cur.EXPECT().All(t.Context(), gomock.Any()).Do(func(ctx context.Context, arg interface{}) {
 			users := arg.(*[]simple.User)
 			*users = append(*users, expectedUsers...)
 		}).Return(nil)
 
 		col := gomockMocks.NewMockCollection(ctrl)
-		col.EXPECT().InsertMany(ctx, gomock.Any()).Return(
+		col.EXPECT().InsertMany(t.Context(), gomock.Any()).Return(
 			&mongo.InsertManyResult{
 				InsertedIDs: []interface{}{
 					primitive.NewObjectID(),
@@ -93,8 +89,8 @@ func TestUsersWorkflow(t *testing.T) {
 			},
 			nil,
 		)
-		col.EXPECT().Find(ctx, gomock.Any()).Return(cur, nil)
-		col.EXPECT().DeleteMany(ctx, gomock.Any()).Return(
+		col.EXPECT().Find(t.Context(), gomock.Any()).Return(cur, nil)
+		col.EXPECT().DeleteMany(t.Context(), gomock.Any()).Return(
 			&mongo.DeleteResult{
 				DeletedCount: 4,
 			},
@@ -110,18 +106,12 @@ func TestUsersWorkflow(t *testing.T) {
 	t.Run("docker", func(t *testing.T) {
 		t.Parallel()
 
-		uri := os.Getenv("MONGO_URI")
-		require.NotEmpty(t, uri)
-
-		opt := options.Client().ApplyURI(uri)
-		cl, err := mongoifc.NewClient(opt)
+		opt := options.Client().ApplyURI(MongoUri)
+		cl, err := mongoifc.Connect(t.Context(), opt)
 		require.NoError(t, err)
 		require.NotNil(t, cl)
-
-		err = cl.Connect(ctx)
-		require.NoError(t, err)
 		t.Cleanup(func() {
-			require.NoError(t, cl.Disconnect(ctx))
+			require.NoError(t, cl.Disconnect(context.Background()))
 		})
 
 		db := cl.Database(fmt.Sprintf("simple_%d", time.Now().Unix()))
@@ -129,24 +119,25 @@ func TestUsersWorkflow(t *testing.T) {
 	})
 }
 
-func usersWorkflow(t testing.TB, db mongoifc.Database) {
-	ctx := context.Background()
-	ids, err := simple.Create(ctx, db,
+func usersWorkflow(tb testing.TB, db mongoifc.Database) {
+	tb.Helper()
+
+	ids, err := simple.Create(tb.Context(), db,
 		simple.User{Name: "blocked admin", Active: false, IsAdmin: true},
 		simple.User{Name: "active non-admin", Active: true, IsAdmin: false},
 		expectedUsers[0],
 		expectedUsers[1],
 	)
-	require.NoError(t, err)
-	require.Len(t, ids, 4)
+	require.NoError(tb, err)
+	require.Len(tb, ids, 4)
 
-	users, err := simple.GetAdmins(ctx, db)
-	require.NoError(t, err)
+	users, err := simple.GetAdmins(tb.Context(), db)
+	require.NoError(tb, err)
 	for i, u := range users {
 		u.ID = ""
 		users[i] = u
 	}
-	require.Equal(t, expectedUsers, users)
+	require.Equal(tb, expectedUsers, users)
 
-	require.NoError(t, simple.Delete(ctx, db, ids...))
+	require.NoError(tb, simple.Delete(tb.Context(), db, ids...))
 }
